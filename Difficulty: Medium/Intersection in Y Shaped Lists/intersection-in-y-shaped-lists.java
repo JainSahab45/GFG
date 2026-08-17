ecca6fd7aa06effca552d4083a2d6688ef88af8e
Tanish Jain <tanishjain10.dit@gmail.com>
2026-08-17 16:38:46 +0530

    Optimize intersection detection in Y-shaped lists
    Optimized the intersection point finding logic for two linked lists.

--- Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.java
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.java
@@ -8,7 +8,21 @@ class Node {
 		next = null;
 	}
 } */
+// optimized one
+if(head1 == null || head2 == null){
+		    return null;
+		}
+		Node temp1 = head1 ;
+		Node temp2 = head2 ;
+		
+		while(temp1 != temp2){
+		    temp1 = (temp1 == null) ? head1 : temp1.next ;
+		    temp2 = (temp2 == null) ? head2 : temp2.next ;
+		}
+		return temp1 ;
 
+
+// code i wrote 
 class Solution {
 	public Node intersectPoint(Node head1, Node head2) {
 		// code here
